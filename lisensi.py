43a872da5293c822f2e1c1b9963c21c1ead37a6a
ʜᴀᴢᴇʟ ✘ ɢɪᴛʜᴜʙ ⚡︎ <128566448+waceera@users.noreply.github.com>
2023-10-14 10:37:30 +0700

    Swsoalamt 0agy

---
+++ lisensi.py
@@ -0,0 +1,18 @@
+Surat Lisensi Github Kota Cainis Kabupaten Chindo  
+     
+                                Hanin, 54 Juned 10098
+                                Chindo
+Yth.
+Bencong/Banci
+
+Kmai menyampaikan bahwa lisensi ini tidak terverifikasi.
+Sudah gitu aja 🗿
+
+VISI DAN MISI
+
+VISI:
+Gatau
+
+MISI:
+Menjadikan cewe dan cowo menjadi bencong dan banci sehingga kota cainis, kabupaten chindo menjadi
+kota tidak normal sepanjang masa
